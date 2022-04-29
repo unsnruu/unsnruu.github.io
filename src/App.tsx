a75f187a2efc10d8a303c0585a68071d5c2ffc8b
unsnruu <unsnruu@gmail.com>
2022-04-29 13:14:21 +0900

    Add global style by emotion

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import React from "react";
-import Header from "./components/Header";
-import Why from "./components/Why";
+import Router from "./pages/Router";
+import { Global, css } from "@emotion/react";
+
+const globalStyles = css`
+  * {
+    margin: 0;
+    box-sizing: border-box;
+  }
+`;
 
 function App() {
   return (
     <div>
-      <Header />
-      <Why />;
+      <Global styles={globalStyles} />
+      <Router />
     </div>
   );
 }
