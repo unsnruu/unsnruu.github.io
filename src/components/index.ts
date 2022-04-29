0ff0245bf8c520d86b3b118332ddec7d50d97a62
unsnruu <unsnruu@gmail.com>
2022-04-29 13:10:58 +0900

    Create index.ts to export together

---
+++ src/components/index.ts
@@ -0,0 +1,21 @@
+import About from "./About";
+import Contact from "./Contact";
+import Experienced from "./Experienced";
+import Header from "./Header";
+import Project from "./Project";
+import Character from "./Character/";
+import Why from "./Why";
+import Footer from "./Footer";
+import Promising from "./Promising";
+
+export {
+  About,
+  Contact,
+  Experienced,
+  Header,
+  Project,
+  Character,
+  Why,
+  Promising,
+  Footer,
+};
