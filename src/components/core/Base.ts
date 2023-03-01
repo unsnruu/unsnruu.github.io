9d94797e856a7b6329a570f1788eb6cbf8d987f7
unsnruu <unsnruu@gmail.com>
2023-03-02 04:39:22 +0900

    ♻️ createStyleProps 함수를 통해서 리팩토링을 진행함

--- src/components/core/Base.ts
+++ src/components/core/Base.ts
@@ -1,15 +1,18 @@
-import Component, { StyleProps } from "@/types/component";
+import Component from "@/types/component";
 import { stringifyStyleProps } from "@/utils/stringifyStyleProps";
+import { createStyleProps } from "@/utils/createStyleProps";
 
-export const baseStyleProps: StyleProps = {
-  margin: "0px",
-  padding: "0px",
-  boxShadow: "none",
-  boxSizing: "border-box",
-  backgroundColor: "inherit",
-  width: "0px",
-  height: "0px",
-};
+const styleProps = createStyleProps({
+  container: {
+    margin: "0px",
+    padding: "0px",
+    boxShadow: "none",
+    boxSizing: "border-box",
+    backgroundColor: "inherit",
+    width: "0px",
+    height: "0px",
+  },
+});
 
 export default class Base extends Component {
   constructor() {
@@ -17,24 +20,24 @@ export default class Base extends Component {
   }
 
   static get observedAttributes(): string[] {
-    return Object.keys(baseStyleProps);
+    return Object.keys(styleProps.container);
   }
 
-  getTemplate(): HTMLElement {
+  getTemplate(): HTMLTemplateElement {
     const template = document.createElement("template");
     template.innerHTML = `
       <div id="container">
         <slot name="children"></slot>
       <div/>
     `.trim();
-    return template.content.cloneNode(true) as HTMLElement;
+    return template;
   }
 
   getStyle(): HTMLStyleElement {
     const style = document.createElement("style");
     style.innerHTML = `
       div#container {
-        ${stringifyStyleProps(this, baseStyleProps)}
+        ${stringifyStyleProps(this, styleProps.container)}
       } 
       `.trim();
 
