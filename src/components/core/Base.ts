26bc8fc72efde2a0d4b604869cc43fa0bebfb1e5
unsnruu <unsnruu@gmail.com>
2023-03-01 14:58:03 +0900

    ✨ Base 컴포넌트 제작

--- src/components/core/Base.ts
+++ src/components/core/Base.ts
@@ -1,20 +1,50 @@
-import Component from "@/types/component";
+import Component, { StyleKeys, StyleProps } from "@/types/component";
+import { stringifyStyleProps } from "@/utils/stringifyStyleProps";
+
+export const baseStyleProps: StyleProps = {
+  margin: "0px",
+  padding: "0px",
+  boxShadow: "none",
+  boxSizing: "border-box",
+  backgroundColor: "inherit",
+  width: "0px",
+  height: "0px",
+};
 
 export default class Base extends Component {
   constructor() {
     super();
   }
-  static get observedAttributes(): string[] {
-    return [];
+
+  static get observedAttributes(): StyleKeys[] {
+    return [
+      "margin",
+      "padding",
+      "boxShadow",
+      "width",
+      "height",
+      "backgroundColor",
+    ];
   }
-  getTemplate(): HTMLTemplateElement {
+
+  getTemplate(): HTMLElement {
     const template = document.createElement("template");
-    template.innerHTML = ``;
-    return template;
+    template.innerHTML = `
+      <div id="container">
+        <slot name="children"></slot>
+      <div/>
+    `;
+    return template.content.cloneNode(true) as HTMLElement;
   }
+
   getStyle(): HTMLStyleElement {
     const style = document.createElement("style");
-    style.innerHTML = ``;
+    style.innerHTML = `
+      div#container {
+        ${stringifyStyleProps(this, baseStyleProps)}
+      } 
+      `.trim();
+
     return style;
   }
 }
