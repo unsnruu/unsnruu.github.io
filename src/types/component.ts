d52df5db9ad44bfb5701910575fc5c851c168a34
unsnruu <unsnruu@gmail.com>
2023-03-01 14:54:39 +0900

    🏷️ component를 생성 시에 사용되는 타입들을 정리
    component를 생성시에 사용하는 기본 구현이 되어 있는 클래스와,
    StyleProps, StyleKey 타입을 정의했습니다.

--- src/types/component.ts
+++ src/types/component.ts
@@ -1,7 +1,13 @@
-export default abstract class Component extends HTMLElement {
+import * as CSS from "csstype";
+export type StyleProps = {
+  [key in keyof CSS.Properties]: CSS.Properties<string, string>[key];
+};
+export type StyleKeys = keyof StyleProps;
+
+export default class Component extends HTMLElement {
   shadowRoot: ShadowRoot;
 
-  static get observedAttributes(): string[] {
+  static get observedAttributes(): StyleKeys[] {
     return [];
   }
   constructor() {
@@ -16,14 +22,21 @@ export default abstract class Component extends HTMLElement {
   }
   disconnectedCallback() {}
   adoptedCallback() {}
-  attributeChangedCallback() {}
+  attributeChangedCallback() {
+    window.requestAnimationFrame(() => {
+      this.render();
+    });
+  }
   addEvents() {}
   render() {
+    this.shadowRoot.innerHTML = "";
     this.shadowRoot.appendChild(this.getStyle());
-    this.shadowRoot.appendChild(this.getTemplate().content.cloneNode(true));
+    this.shadowRoot.appendChild(this.getTemplate());
   }
-  getTemplate(): HTMLTemplateElement {
-    return document.createElement("template");
+  getTemplate(): HTMLElement {
+    return document
+      .createElement("template")
+      .content.cloneNode(true) as HTMLElement;
   }
   getStyle(): HTMLStyleElement {
     return document.createElement("style");
