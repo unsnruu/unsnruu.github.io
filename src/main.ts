3ab8d8d4a166d0e8bc78da54a5141d05d0e2b307
unsnruu <unsnruu@gmail.com>
2023-03-02 23:27:32 +0900

    ✨ main에 웹 컴포넌트 정의

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,9 @@
-document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
-<h1>Hello</h1>
-`;
+import Base from "./components/core/Base";
+import Text from "./components/core/Text";
+import Flexbox from "./components/core/Flexbox";
+import FlexItem from "./components/core/FlexItem";
+
+customElements.define("core-base", Base);
+customElements.define("core-text", Text);
+customElements.define("flex-box", Flexbox);
+customElements.define("flex-item", FlexItem);
