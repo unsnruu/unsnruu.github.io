8eab537c38412157310f16e7c85f33c99d4e94a0
unsnruu <unsnruu@gmail.com>
2023-03-02 15:35:31 +0900

    ✨ FlexItem 컴포넌트 생성

---
+++ src/components/core/FlexItem.ts
@@ -0,0 +1,33 @@
+import FlexBox, { styleProps as flexStyleProps } from "./FlexBox";
+import { createStyleProps } from "@/utils/createStyleProps";
+import { stringifyStyleProps } from "@/utils/stringifyStyleProps";
+
+/**
+ * FlexItem은 단순히 FlexBox의 slotted CSS 연산자를 이용하기 위한 컴포넌트입니다.
+ * FlexItem을 사용하면 FlexBox와 사용했을 때 자동으로
+ * flex 값이 1로 설정되어서 가득 찬 화면을 구성할 수 있습니다.
+ */
+
+const styledProps = createStyleProps({
+  container: {
+    ...flexStyleProps.container,
+  },
+});
+
+export default class FlexItem extends FlexBox {
+  constructor() {
+    super();
+  }
+  getTemplate(): string {
+    return `
+    <div id="container">
+      <slot name="children"></slot>
+    </div>
+    `;
+  }
+  getStyle(): string {
+    return `
+    div#container{${stringifyStyleProps(this, styledProps.container)}}
+    `;
+  }
+}
