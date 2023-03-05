90e2a4ad798e834dd7081895652950e9910421f2
unsnruu <unsnruu@gmail.com>
2023-03-06 02:13:30 +0900

    ✨ core/index.ts를 통해서 컴포넌트를 관리하고 main에서 import 수행

---
+++ src/components/core/index.ts
@@ -0,0 +1,11 @@
+import Base from "./Base";
+import Button from "./Button";
+import Flexbox from "./Flexbox";
+import FlexItem from "./FlexItem";
+import Text from "./Text";
+
+customElements.define("core-base", Base);
+customElements.define("core-button", Button);
+customElements.define("flex-box", Flexbox);
+customElements.define("flex-item", FlexItem);
+customElements.define("core-text", Text);
