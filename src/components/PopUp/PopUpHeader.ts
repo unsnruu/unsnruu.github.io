0647ecc147e9390e50002101cf95b4b95304830e
unsnruu <unsnruu@gmail.com>
2023-03-09 14:09:21 +0900

    ✨ PopUpHeader의 UI 로직을 변경
    기존에는 text를 별도의 slot을 통해서 받았더라면,
    text를 attribute를 통해서 받습니다.
    그 외에 기본적으로 아이콘을 설정했습니다.

--- src/components/PopUp/PopUpHeader.ts
+++ src/components/PopUp/PopUpHeader.ts
@@ -3,15 +3,16 @@ import { createStyleProps } from "@/utils/createStyleProps";
 import { stringifyStyleProps } from "@/utils/stringifyStyleProps";
 
 const styleProps = createStyleProps({
-  container: {
-    width: "100%",
+  host: {
+    display: "block",
+    width: "max-content",
     backgroundColor: "var(--main-blue)",
     fontFamily: "'Press Start 2P', cursive",
     color: "white",
+    padding: "0.5rem",
   },
   slot: {
     display: "block",
-    padding: "8px",
     paddingLeft: "1rem",
   },
 });
@@ -29,15 +30,26 @@ export default class PopUpHeader extends Base {
 
   getTemplate(): string {
     return `
-    <div id="container">
-      <slot name="children"/>
-    </div>
+    <flex-box width="400px" justifyContent="flex-start">
+      <flex-item justifyContent="flex-start">
+        <div slot="children">
+          ${this.title}
+        </div>
+      </flex-item>
+      <flex-item justifyContent="flex-end">
+        <flex-box slot="children" id="icon-container">
+          <flex-item><core-button slot="children" src="/icon-minimize.png"/></flex-item>
+          <flex-item><core-button slot="children" src="/icon-maximize.png"/></flex-item>
+          <flex-item><core-button slot="children" src="/icon-cancel.png"/></flex-item>
+        </flex-box>
+      </flex-item>
+    </flex-bot>
     `;
   }
   getStyle(): string {
     return `
-    #container{${stringifyStyleProps(this, styleProps.container)}}
-    ::slotted(*){${stringifyStyleProps(this, styleProps.slot)}}
+    :host{${stringifyStyleProps(this, styleProps.host)}}
+    
     `;
   }
 }
