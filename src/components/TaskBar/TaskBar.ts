c97f0a700c9cf7883bbe3c6408d5f5ff3aefc212
unsnruu <unsnruu@gmail.com>
2023-03-09 14:52:47 +0900

    💄 TaskBar의 스타일 추가

--- src/components/TaskBar/TaskBar.ts
+++ src/components/TaskBar/TaskBar.ts
@@ -1,13 +1,31 @@
 import Component from "@/types/component";
+import { createStyleProps } from "@/utils/createStyleProps";
+import { stringifyStyleProps } from "@/utils/stringifyStyleProps";
+
+const styleProps = createStyleProps({
+  host: {
+    position: "absolute",
+    bottom: "0",
+    display: "block",
+    width: "100vw",
+    height: "32px",
+    backgroundColor: "var(--main-gray)",
+    borderTop: "1px solid white",
+    outline: "1px solid var(--main-gary)",
+  },
+});
 
 export default class TaskBar extends Component {
   constructor() {
     super();
   }
   getTemplate(): string {
-    return ``;
+    return `
+    <div></div>
+    `;
   }
   getStyle(): string {
-    return ``;
+    return `
+    :host{${stringifyStyleProps(this, styleProps.host)}}`;
   }
 }
