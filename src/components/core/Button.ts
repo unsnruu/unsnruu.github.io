c44c6c4e56a0b9dda1573cdfb992c8a2909f2601
unsnruu <unsnruu@gmail.com>
2023-03-09 13:16:03 +0900

    ✨ img를 대응할 수 있도록 로직 변경

--- src/components/core/Button.ts
+++ src/components/core/Button.ts
@@ -5,7 +5,27 @@ import { stringifyStyleProps } from "@/utils/stringifyStyleProps";
 const styleProps = createStyleProps({
   container: {
     ...baseStyleProps.container,
-    outline: "none",
+    width: "min-content",
+    height: "min-content",
+    border: "2px outset white",
+    borderBottom: "2px solid black",
+    borderRight: "2px solid black",
+    background: "var(--main-gray)",
+  },
+  containerActive: {
+    border: "2px inset white",
+    borderTop: "2px solid black",
+    borderLeft: "2px solid black",
+  },
+  slotText: {
+    width: "100%",
+    height: "100%",
+    borderBottom: "1px solid  var(--main-darkgray)",
+    borderRight: "1px solid var(--main-darkgray)",
+    overflow: "hidden",
+  },
+  img: {
+    padding: "0.25rem",
   },
 });
 
@@ -16,18 +36,35 @@ export default class Button extends Base {
   static get observedAttributes(): string[] {
     return [];
   }
+  get text() {
+    console.log(this.getAttribute("text"));
+    return this.getAttribute("text") ?? "";
+  }
+  get src() {
+    return this.getAttribute("src") ?? "";
+  }
+  isImageElement() {
+    return this.src.length > 0;
+  }
   getTemplate(): string {
     return `
-    <div> id="container">
-      <slot name="children"></slot>
+    <div id="container"> 
+      <flex-box width="100%" height="100%">
+        <flex-item widht="100%" height="100%">
+          <div id="slot-text" slot="children">
+          ${this.text}
+          ${this.isImageElement() && `<img id="img" src=${this.src}>`}
+        </flex-item>
+      </flex-box>
     </div>
     `;
   }
   getStyle(): string {
     return `
-    #container{
-      ${stringifyStyleProps(this, styleProps.container)}
-    }
+    #container{${stringifyStyleProps(this, styleProps.container)}}
+    #container:active{${stringifyStyleProps(this, styleProps.containerActive)}}
+    #slot-text{${stringifyStyleProps(null, styleProps.slotText)}}
+    #img{${stringifyStyleProps(null, styleProps.img)}}
     `;
   }
 }
