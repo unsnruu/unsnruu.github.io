5e1e6102b059a6b21f929d31282eeef1faf0aa36
unsnruu <unsnruu@gmail.com>
2023-03-11 23:01:30 +0900

    [#10] 🚚 Base의 스타일을 별도의 파일로 분리

--- src/components/core/Base/index.ts
+++ src/components/core/Base/index.ts
@@ -1,20 +1,10 @@
-import { LitElement, css, html } from "lit";
+import { LitElement, html } from "lit";
 import { customElement } from "lit/decorators";
+import { baseStyle } from "./index.styles";
 
 @customElement("core-base")
 export class Base extends LitElement {
-  static styless = css`
-    :host {
-      margin: 0px;
-      padding: 0px;
-      box-shadow: none;
-      box-sizing: border-box;
-      background: inherit;
-      border: none;
-      width: auto;
-      height: auto;
-    }
-  `;
+  static styles = baseStyle;
 
   render() {
     return html`<slot></slot>`;
