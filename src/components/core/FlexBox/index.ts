f93f232286f2a53735e64a6d513855e4cf592977
unsnruu <unsnruu@gmail.com>
2023-03-11 23:06:40 +0900

    [#10] ♻️ FlexBox를 lit을 통해서 refactor

---
+++ src/components/core/FlexBox/index.ts
@@ -0,0 +1,24 @@
+import { LitElement, css, html } from "lit";
+import { customElement } from "lit/decorators";
+import { baseStyle } from "../Base/index.styles";
+
+@customElement("flex-box")
+export class FlexBox extends LitElement {
+  static styles = [
+    baseStyle,
+    css`
+      host {
+        display: flex;
+        width: max-content;
+        flex-wrap: wrap;
+        justify-content: center;
+        align-items: center;
+        flex-direction: row;
+        gap: 0;
+      }
+    `,
+  ];
+  render() {
+    return html`<slot name="flex-item"></slot> `;
+  }
+}
