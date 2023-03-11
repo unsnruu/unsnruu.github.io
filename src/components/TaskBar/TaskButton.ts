1499234484a17b3d9184823478d09b797ddd05a8
unsnruu <unsnruu@gmail.com>
2023-03-11 12:43:14 +0900

    [#3] ✨ title 값이 attribute로 존재하는 경우만 렌더링하도록 로직 추가

--- src/components/TaskBar/TaskButton.ts
+++ src/components/TaskBar/TaskButton.ts
@@ -4,9 +4,14 @@ export default class TaskButton extends Component {
   constructor() {
     super();
   }
+  get title() {
+    return this.getAttribute("title") ?? "";
+  }
   getTemplate(): string {
+    if (!this.title) return ``;
+
     return `
-      <core-button text="abcde" height="32px"></core-button>
+      <core-button text="${this.title}" hgeight="32px"></core-button>
     `;
   }
   getStyle(): string {
