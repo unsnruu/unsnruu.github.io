369b87573d4ccfdcf39f7c4e947a1c4c0084034c
unsnruu <unsnruu@gmail.com>
2023-03-12 00:24:31 +0900

    [#10] ✨ 정의된 컴포넌트들의 import하도록 함

--- src/components/core/index.ts
+++ src/components/core/index.ts
@@ -1,11 +1,4 @@
-import Base from "./Base";
-import Button from "./Button";
-import Flexbox from "./Flexbox";
-import FlexItem from "./FlexItem";
-import Text from "./Text";
-
-customElements.define("core-base", Base);
-customElements.define("core-button", Button);
-customElements.define("flex-box", Flexbox);
-customElements.define("flex-item", FlexItem);
-customElements.define("core-text", Text);
+import "./Base";
+import "./Button";
+import "./FlexBox";
+import "./FlexItem";
