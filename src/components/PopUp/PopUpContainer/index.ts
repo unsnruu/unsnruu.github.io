9b519397855938bb31c565b9c7ed929397aa96df
unsnruu <unsnruu@gmail.com>
2023-03-12 12:24:44 +0900

    [#10] ♻️ PopUp을 PopUpContainer로 lit을 사용해 refactor

---
+++ src/components/PopUp/PopUpContainer/index.ts
@@ -0,0 +1,40 @@
+import { LitElement, html, css } from "lit";
+import { customElement, property } from "lit/decorators";
+import { baseStyle } from "@/components/core/Base/index.styles";
+
+@customElement("pop-up")
+export class PopUp extends LitElement {
+  @property() top = 0;
+  @property() left = 0;
+  @property() header = "";
+
+  static styles = [
+    baseStyle,
+    css`
+      :host {
+        position: absolute;
+        top: 0px;
+        left: 0px;
+        background-color: var(--main-gray);
+        border: 2px outset gray;
+      }
+      #container {
+        height: 100%;
+        box-sizing: border-box;
+        padding: 0.25rem;
+        border: 1px outset white;
+      }
+    `,
+  ];
+
+  render() {
+    return html`
+      <div id="container">
+        <pop-up-header></pop-up-header>
+        <pop-up-body>
+          <slot name="body"></slot>
+        </pop-up-body>
+      </div>
+    `;
+  }
+}
