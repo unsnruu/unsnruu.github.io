2f44ec27537c7e343187374bcd6457f1fc5a0158
unsnruu <unsnruu@gmail.com>
2023-03-13 00:00:02 +0900

    [#10] ✨ FlexItem의 스토리를 작성

--- src/components/core/FlexItem/index.stories.ts
+++ src/components/core/FlexItem/index.stories.ts
@@ -1,11 +1,12 @@
-// import { Story, Meta } from "@storybook/web-components";
-// import { html } from "lit-html";
+import { Story, Meta } from "@storybook/web-components";
+import { html } from "lit-html";
 
-// import "./index";
+import "./index";
+import { FlexItem } from "./index";
 
-// export default {
-//   title: "Core/FlexItem",
-// } as Meta;
+export default {
+  title: "Core/FlexItem",
+} as Meta;
 
-// export const Default: Story = () =>
-//   html`<flex-item flex="1"><div>hey</div></flex-item>`;
+export const Default: Story<FlexItem> = () =>
+  html`<flex-item><span>abcde</span></flex-item>`;
