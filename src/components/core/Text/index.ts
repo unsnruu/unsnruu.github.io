78c097b6bcc44dd95b0cb335d5aff92d57b212ca
unsnruu <unsnruu@gmail.com>
2023-03-12 12:47:17 +0900

    [#10] 🐛 import문의 확장자에 js를 붙임

--- src/components/core/Text/index.ts
+++ src/components/core/Text/index.ts
@@ -1,5 +1,5 @@
 import { LitElement, html, css } from "lit";
-import { customElement, property } from "lit/decorators";
+import { customElement, property } from "lit/decorators.js";
 import { baseStyle } from "../Base/index.styles";
 
 @customElement("core-text")
