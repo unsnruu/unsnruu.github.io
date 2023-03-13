be1300f1710b0ceecb836535fc1ae3e93f4bcedc
unsnruu <unsnruu@gmail.com>
2023-03-13 14:52:57 +0900

    [#10] ✨ 새로운 WihtinFlexbox 스토리가 추가

--- src/components/core/FlexItem/index.stories.ts
+++ src/components/core/FlexItem/index.stories.ts
@@ -1,12 +1,23 @@
 import { Story, Meta } from "@storybook/web-components";
 import { html } from "lit-html";
 
-import "./index";
+import "../index";
 import { FlexItem } from "./index";
 
 export default {
   title: "Core/FlexItem",
 } as Meta;
 
-export const Default: Story<FlexItem> = () =>
-  html`<flex-item><span>abcde</span></flex-item>`;
+export const Default: Story<FlexItem> = () => {
+  return html`<flex-item><span>Item</span></flex-item>`;
+};
+
+export const WithinFlexbox: Story<FlexItem> = () => {
+  return html`
+    <flex-box>
+      <flex-item .flex=${1}>item1</flex-item>
+      <flex-item .flex=${2}>item2</flex-item>
+      <flex-item .flex=${3}>item3</flex-item>
+    </flex-box>
+  `;
+};
