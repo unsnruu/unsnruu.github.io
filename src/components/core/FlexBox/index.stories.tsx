fd50ce9f35ef79dbe2c7a4da48ba018aa28ae15f
unsnruu <unsnruu@gmail.com>
2023-03-15 02:38:03 +0900

    [#10] ✨ justifyContent와 alignItems argTypes 설정

--- src/components/core/FlexBox/index.stories.tsx
+++ src/components/core/FlexBox/index.stories.tsx
@@ -6,12 +6,60 @@ import { FlexBox } from "./index";
 
 export default {
   title: "Core/FlexBox",
-} as Meta;
+  argTypes: {
+    justifyContent: {
+      control: "select",
+      options: [
+        "-moz-initial",
+        "inherit",
+        "initial",
+        "revert",
+        "revert-layer",
+        "unset",
+        "space-around",
+        "space-between",
+        "space-evenly",
+        "stretch",
+        "center",
+        "end",
+        "flex-end",
+        "flex-start",
+        "start",
+        "left",
+        "normal",
+        "right",
+      ],
+    },
+    alignItems: {
+      control: "select",
+      options: [
+        "-moz-initial",
+        "inherit",
+        "initial",
+        "revert",
+        "revert-layer",
+        "unset",
+        "stretch",
+        "center",
+        "end",
+        "flex-end",
+        "flex-start",
+        "start",
+        "normal",
+        "self-end",
+        "self-start",
+        "baseline",
+      ],
+    },
+  },
+} as Meta<FlexBox>;
 
-export const Default: Story<FlexBox> = () => {
-  return html` <flex-box>
-    <flex-item><span>Item1</span></flex-item>
-    <flex-item><span>Item2</span></flex-item>
-    <flex-item><span>Item3</span></flex-item>
-  </flex-box>`;
+export const Default: Story<FlexBox> = ({ justifyContent, alignItems }) => {
+  return html`
+    <flex-box .justifyContent=${justifyContent} .alignItems=${alignItems}>
+      <flex-item><span>Item1</span></flex-item>
+      <flex-item><span>Item2</span></flex-item>
+      <flex-item><span>Item3</span></flex-item>
+    </flex-box>
+  `;
 };
