496864376538b1c41765196dba3ffb33054c6033
unsnruu <unsnruu@gmail.com>
2023-03-21 15:10:05 +0900

    [#13] 🔥 : 불필요한 코드 제거

--- src/types/styleProp.ts
+++
@@ -1,5 +0,0 @@
-import * as CSS from "csstype";
-
-export type StyleProp = {
-  [Key in keyof CSS.Properties]: CSS.Properties<string, string>[Key];
-};
--- src/utils/createStyleProps.ts
+++
@@ -1,7 +0,0 @@
-import { StyleProp } from "@/types/styleProp";
-
-type StyleProps<T> = {
-  [K in keyof T]: StyleProp;
-};
-
-export const createStyleProps = <T>(styleProps: StyleProps<T>) => styleProps;
