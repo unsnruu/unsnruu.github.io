21824ba5245da30549c0450e0446e33b90785736
unsnruu <unsnruu@gmail.com>
2023-03-31 10:54:17 +0900

    [#23] ✨ : 앱 초기 상태를 생성하는 generateAppState 함수 분리

---
+++ src/utils/generateAppState.ts
@@ -0,0 +1,16 @@
+import { nanoid } from "nanoid";
+import type { ApplicationState } from "@/types/ApplicationState";
+
+type ApplicationContent = Pick<ApplicationState, "appName" | "content">;
+
+export const generateAppState = ({
+  appName,
+  content,
+}: ApplicationContent): ApplicationState => ({
+  id: nanoid(),
+  appName,
+  content,
+  isOpen: true,
+  maximize: false,
+  minimize: false,
+});
