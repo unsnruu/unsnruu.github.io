9891483cd1e101b0b846513eb90999a7274644df
unsnruu <unsnruu@gmail.com>
2023-03-31 12:43:44 +0900

    [#22] 📦️ : throttle-debounce 라이브러리 설치 및 적용

--- src/model/store.ts
+++ src/model/store.ts
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { executionContextReducer } from "./executionContextSlice";
+import logger from "redux-logger";
 
 export const store = configureStore({
   reducer: {
     executionContext: executionContextReducer,
   },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
